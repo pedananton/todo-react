d73f92faea47374af0a8b0d4054196054a838c9f
Anton Pedan <caravellejetta@gmail.com>
2021-06-15 13:13:46 +0300

    Updates

---
+++ precache-manifest.0ea9307092cb0f1c2bfecb81fb085b73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6f8dae509b951ef456396cf187b5aec",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "ea373faadf3f4d0c0a26",
+    "url": "/todo-react/static/css/main.c01547e7.chunk.css"
+  },
+  {
+    "revision": "899712c8debbce5cb876",
+    "url": "/todo-react/static/js/2.a277081d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.a277081d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea373faadf3f4d0c0a26",
+    "url": "/todo-react/static/js/main.a42afcd1.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
